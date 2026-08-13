367fb3e059c3d6faf171768cd3c30b0bdb661f1b
Lovro <lovro.posaric@gmail.com>
2026-08-14 00:40:33 +0200

    Improve the screenshot score mod
    - no more 8 second blocking of the tick thread
    - capturing state is moved into the mod itself
    - corrected IsEnabled() gating
    - cancels if the current menu ends up not being the correct one

--- DLL/Mods/ScreenShotScoresMod.cpp
+++ DLL/Mods/ScreenShotScoresMod.cpp
@@ -5,13 +5,45 @@
 using Framework::ModContext;
 namespace Setting = Settings::Setting;
 
+bool ScreenShotScoresMod::IsEnabled(const ModContext& c) const {
+	return c.IsOn(Setting::ScreenShotScores);
+}
+
+void ScreenShotScoresMod::OnDisabled(ModContext&) {
+	ResetCapture();
+}
+
 void ScreenShotScoresMod::OnMenuTick(ModContext& c) {
-	if (c.IsOn(Setting::ScreenShotScores) && GameState::Menus::IsInScoreMenus()) {
+	if (!GameState::Menus::IsInScoreMenus()) {
+		if (!scoreMenu.empty()) {
+			ResetCapture();
+		}
+		
+		return;
+	}
+
+	const auto now = std::chrono::steady_clock::now();
+	if (scoreMenu != GameState::currentMenu) {
+		scoreMenu = GameState::currentMenu;
+		captureAt = now + std::chrono::seconds(8);
+		captured = false;
+		return;
+	}
+
+	if (!captured && now >= captureAt) {
 		Keyboard::TakeScreenshot();
+		captured = true;
 	}
-	else {
-		Keyboard::takenScreenshotOfThisScreen = false;
-	}
+}
+
+void ScreenShotScoresMod::OnSongEnter(ModContext&) {
+	ResetCapture();
+}
+
+void ScreenShotScoresMod::ResetCapture() {
+	scoreMenu.clear();
+	captureAt = {};
+	captured = false;
 }
 
 static Framework::ModRegistrar<ScreenShotScoresMod> _screenShotScoresReg;
--- DLL/Mods/ScreenShotScoresMod.hpp
+++ DLL/Mods/ScreenShotScoresMod.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <chrono>
+#include <string>
+
 #include "../Framework/Framework.hpp"
 
 class ScreenShotScoresMod : public Framework::IMod {
 public:
 	MOD_ID(ScreenShotScoresMod)
+	bool IsEnabled(const Framework::ModContext& c) const override;
 
+	void OnDisabled(Framework::ModContext& c) override;
 	void OnMenuTick(Framework::ModContext& c) override;
+	void OnSongEnter(Framework::ModContext& c) override;
+
+private:
+	void ResetCapture();
+
+	std::string scoreMenu;
+	std::chrono::steady_clock::time_point captureAt{};
+	bool captured = false;
 };
